53f475bffd8d7290d536548f91a9aeba6fb16545
kwonih1020 <kwonih1020@gmail.com>
2022-08-03 14:00:09 +0900

    redux logger 적용

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,14 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { combineReducers } from "redux";
 import todos from "../modules/todo";
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
   todos: todos
 });
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+);
 
 export default store;
