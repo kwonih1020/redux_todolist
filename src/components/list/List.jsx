4664749a6d9fa6a25df07fbdd2911a82f533f30d
kwonih1020 <kwonih1020@gmail.com>
2022-08-04 00:47:28 +0900

    todo.id => i 로 변경

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,74 +1,88 @@
-import React from 'react';
-import { ListBoxDone, ListBoxTodo, ListContainer, TodoContainer, ButtonBox, Button } from './style.js';
+import React from "react";
+import {
+  ListBoxDone,
+  ListBoxTodo,
+  ListContainer,
+  TodoContainer,
+  ButtonBox,
+  Button,
+} from "./style.js";
 import { useSelector } from "react-redux";
-import { deleteTodo, updateTodo } from '../../redux/modules/todo.js';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
-
+import { deleteTodo, updateTodo } from "../../redux/modules/todo.js";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const List = () => {
   const lists = useSelector((state) => state.todos.todos);
-  console.log(lists);
-  
+  // console.log(lists);
+
   const dispatch = useDispatch();
 
   // 삭제
   const deleteHandler = (e, id) => {
     e.preventDefault();
-    dispatch(deleteTodo(id))
-  }
+    dispatch(deleteTodo(id));
+  };
 
   // 완료하기
   const updateHandler = (id) => {
     dispatch(updateTodo(id));
-    console.log(lists)
-  }
+    console.log(lists);
+  };
 
   return (
     <ListContainer>
-      <h1 className='list-title'>Working!</h1>
+      <h1 className="list-title">Working!</h1>
       <ListBoxTodo>
-        {lists.map((todo, i) => (
-          !todo.isDone ?
+        {lists.map((todo, i) =>
+          !todo.isDone ? (
             <TodoContainer key={i}>
-              <Link to={`/detail/${todo.id}`} 
-                    style={{ textDecoration: 'none', color: "red" }}>
-                    상세페이지
+              <Link
+                to={`/detail/${i}`}
+                style={{ textDecoration: "none", color: "red" }}>
+                상세페이지
               </Link>
               <div>
                 <h4>{todo.title}</h4>
                 <h4>{todo.body}</h4>
                 <ButtonBox>
-                  <Button onClick={(e) => deleteHandler(e, todo.id)}>삭제하기</Button>
-                  <Button onClick={() => updateHandler(todo.id)}>완료하기</Button>
+                  <Button onClick={(e) => deleteHandler(e, todo.id)}>
+                    삭제하기
+                  </Button>
+                  <Button onClick={() => updateHandler(todo.id)}>
+                    완료하기
+                  </Button>
                 </ButtonBox>
               </div>
             </TodoContainer>
-          : null
-        ))}
+          ) : null
+        )}
       </ListBoxTodo>
-      <h1 className='list-title'>Done!</h1>
+      <h1 className="list-title">Done!</h1>
       <ListBoxDone>
-        {lists.map((todo, i) => (
-          todo.isDone ?
+        {lists.map((todo, i) =>
+          todo.isDone ? (
             <TodoContainer key={i}>
-              <Link to={`/detail/${todo.id}`} 
-                    style={{ textDecoration: 'none', color: "red" }}>
-                    상세페이지
+              <Link
+                to={`/detail/${i}`}
+                style={{ textDecoration: "none", color: "red" }}>
+                상세페이지
               </Link>
               <div>
                 <h4>{todo.title}</h4>
                 <h4>{todo.body}</h4>
                 <ButtonBox>
-                  <Button onClick={(e) => deleteHandler(e, todo.id)}>삭제하기</Button>
+                  <Button onClick={(e) => deleteHandler(e, todo.id)}>
+                    삭제하기
+                  </Button>
                   <Button onClick={() => updateHandler(todo.id)}>
                     {todo.isDone ? "취소하기" : "완료하기"}
                   </Button>
                 </ButtonBox>
               </div>
             </TodoContainer>
-          : null
-        ))}
+          ) : null
+        )}
       </ListBoxDone>
     </ListContainer>
   );
